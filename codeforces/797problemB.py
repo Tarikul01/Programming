043c8be72e7c75a7e17df53614242e59f9fd3e7a
Tarikul islam <tarikul.cse5.bu@gmail.com>
2022-07-04 10:09:19 +0600

    Problem B 797B

--- codeforces/797problemB.py
+++ codeforces/797problemB.py
@@ -1,13 +1,20 @@
 # n=int(input())
 # a=list(map(int,input().split()))
-a=[-2,2,-3,1];
-s,en=0,10001
-
-for x in a:
-    if x>0:
-        s+=x
-        # print(x)
-    if x%2!=0:
-        en=min(en,abs(x))
-if s%2==0: s-=en
-print(s)
+# a=[-2,2,-3,1];
+# s,en=0,10001
+#
+# for x in a:
+#     if x>0:
+#         s+=x
+#         # print(x)
+#     if x%2!=0:
+#         en=min(en,abs(x))
+# if s%2==0: s-=en
+# print(s)
+for i in range(20):
+    print("I Love you So much !")
+for i in range(10):
+    print("I miss you so much !")
+for i in range(5):
+    print("Tomake khub   dhorkar")
+print("Amake ektu jhoraia dhorba ekhon..Tomake khub i miss kortechi sona ")
