453b49a021866d380c7723b5abf587220bd48e80
Tarikul islam <tarikul.cse5.bu@gmail.com>
2022-12-08 10:51:16 +0600

    codefource solutions

--- codeforces/main.py
+++ codeforces/main.py
@@ -3,32 +3,8 @@ from itertools import combinations, permutations
 
 
 def solve():
-   # n,m=getInt()
-   # for i in range(m):
-   #     x,y=getInt()
-   # if n>m:
-   #     print("YES")
-   # else:
-   #     print("NO")
+    n=input()
 
-   # n=int(input())
-   # monster=getInt()
-   # spell=getInt()
-   # spell.sort()
-   # print(sum(monster)+sum(spell[0:len(spell)-1]))
-
-   n=int(input())
-   arr=getInt()
-   arr1=sorted(arr)
-   val=arr1[len(arr)-1]
-   ind=arr.index(val)
-   if len(arr)==1:
-       print(arr[0])
-       return
-   if(val>=val-ind+1):
-       print(val)
-   else:
-       print(0)
 
 
 
