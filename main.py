dcfab0792e0cfd4e2dab83c835bd25d4f75e2d4e
Tarikul <tarikul.cse5.bu@gmail.com>
2023-02-05 09:12:09 +0600

    Codeforce problem  D solve

--- main.py
+++ main.py
@@ -16,76 +16,20 @@ mi = lambda: map(int, input().split())
 li = lambda: list(mi())
 
 def solution():
-    n=int(input())
-    # str=input()
-    # arr=[]
-    # tmp=0
-    # for i in range(n):
-    #     if len("".join(set(str[:i+1])))<i+1:
-    #         tmp=i
-    #         break
-    # print(len("".join(set(str[:tmp])))+len("".join(set(str[tmp:]))))
+    n = int(input())
+    s = input()
+    cnt = [0] * len(s)
+    curr = set()
+    for i in range(len(s)):
+        curr.add(s[i])
+        cnt[i] = len(curr)
+    curr2 = set()
+    ans = 0
+    for i in reversed(range(1, len(s))):
+        curr2.add(s[i])
+        ans = max(ans, len(curr2) + cnt[i - 1])
+    print(ans)
 
-    # if n==1:
-    #     print(1)
-    #     return
-    # elif n==0:
-    #     print(0)
-    #     return
-    #
-    # l=0
-    # r=len(str)-1
-    # while (str[l]=='0' and str[r]=='1' and l<=r) or (str[l]=='1' and str[r]=='0' and l<=r):
-    #     l=l+1
-    #     r=r-1
-    # print(len(str[l:r+1]))
-
-
-
-
-
-    # u=0
-    # r=0
-    # for  i in range(n):
-    #
-    #     if str[i]=="U":
-    #         u=u+1
-    #     elif str[i]=="D":
-    #         u=u-1
-    #     elif str[i]=="R":
-    #         r=r+1
-    #     elif str[i]=="L":
-    #         r=r-1
-    #
-    #     if u == 1 and r == 1:
-    #         print("YES")
-    #         return
-    # print("NO")
-
-    # str="codeforces"
-    # ch=input()
-    # if ch in str:
-    #     print("YES")
-    # else:
-    #     print("NO")
-    # sm=sum(arr)
-    arr=li()
-    check=0
-    check1=0
-    sm=sum(arr)
-    for i in range(n-1):
-        if arr[i]==-1 and arr[i]==arr[i+1]:
-            check=1
-        if arr[i]==-1:
-            check1=1
-    if arr[n-1]==-1:
-        check1=1
-    if check==1:
-        print(sm+4)
-    elif check1==1:
-        print(sm)
-    else:
-        print(sm-4)
 
 
 
